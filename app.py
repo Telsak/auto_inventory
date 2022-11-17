48fbce51dbc8310711c8304413d5e10fda06ab17
Simon Olofsson <telsak@hotmail.se>
2022-11-17 11:07:52 +0100

    requirements and tweaks

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 from openpyxl import load_workbook
-import os, json, requests
+import os, json, requests, warnings, csv
+from datetime import date
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['CURRENTROOM'] = ''
+app.config['CURRENTROOM'] = 'B112'
+warnings.simplefilter(action='ignore', category=UserWarning)
+wb = load_workbook(os.path.join(basedir, 'CNAP_utrustning_inventarie.xlsx'))
+ws = wb['Inventarie']
 
 def verify_scantoken(token_type, token):
     '''verifies the token or key against the user json file'''
@@ -33,22 +37,34 @@ def verify_barcode(payload):
         elif payload[0:2] == 'IT' and payload[2:].isnumeric():
             return True, 'IT'
         else:
-            return False, ''
+            return False, 'invalid'
     else:
         # neither of these, or corrupt data
-        return False, ''
+        return False, 'invalid'
 
 def cnap_to_itnumber(cnapdata):
-    wb = load_workbook(os.path.join(basedir, 'CNAP_utrustning_inventarie.xlsx'))
-    ws = wb['Inventarie']
     for row in ws['D']:
         if cnapdata == row.value:
             it_cell = ws[f'E{row.row}'].value
+            if it_cell is None:
+                return 'Not found'
             if len(it_cell) > 0:
                 if 'IT' in it_cell:
-                    ifttt_message(it_cell, verify_scantoken('IFTTT_key', 'NULL'))
-    wb.close()
-    return
+                    return it_cell
+    return 'Not found'
+
+def today():
+    today = date.today()
+    return today.strftime('%Y-%m-%d')
+
+def add_resource_to_csv(itnr):
+    add_header = False if os.path.exists(f'{app.config["CURRENTROOM"]}.csv') else True
+    with open(f'{app.config["CURRENTROOM"]}.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        data = [today(), itnr.rstrip('\n')]
+        if add_header:
+            writer.writerow(['Datum', 'Resurs'])
+        writer.writerow(data)
 
 @app.route('/')
 def index():
@@ -62,22 +78,27 @@ def scan(token='', data=''):
             from flask import current_app
             room = current_app.config['CURRENTROOM'] = data.split(':')[1]
             ifttt_message(f'room changed to {room}', verify_scantoken('IFTTT_key', 'NULL'))
+            return f'room changed to {room}'
         elif 'IT' in data or 'CNAP' in data:
-            # ITnr, check it and process if valid
+            # CNAP and ITnr, check it and process if valid
             result, verify = verify_barcode(data)
             if result:
-                ifttt_message(data, verify_scantoken('IFTTT_key', 'NULL'))
                 if verify == 'CNAP':
-                    cnap_to_itnumber(data)
-                    return(f'{verify.upper()} code', data)
+                    itnr = cnap_to_itnumber(data)
+                    if len(itnr) > 0:
+                        add_resource_to_csv(itnr)
+                        return itnr
+                    else:
+                        return 'No mapping of CNAP-to-IT'
                 else:
-                    return(f'{verify.upper()} code', data)
+                    add_resource_to_csv(data)
+                    return data
             else:
-                return(f'token ok: {data} invalid')
+                return f'{data} invalid'
         else:
-            return f'token ok: {data} invalid'
+            return f'{data} invalid'
     else:
-        return 'failure'
+        return 'invalid token'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
