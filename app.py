b9a5fb0afe8f78043e8e79307659bc530294891a
Simon <telsak@gmail.com>
2022-11-17 06:15:51 +0100

    tokens saved in a single file

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
-import os
+import os, json
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-def verify_input(data):
-    if len(data) > 0:
-        with open(os.path.join(basedir, 'tk.crd')) as file:
-            if data == file.readline():
-                return True
+def verify_scantoken(token_type, token):
+    '''verifies the token or key against the user json file'''
+    if len(token) > 0:
+        with open(os.path.join(basedir, 'users.crd')) as file:
+            json_data = json.load(file)
+            for user in json_data["user_tokens"]:
+                if user[token_type] == token:
+                    return True
     return False
 
 @app.route('/')
@@ -18,10 +21,10 @@ def index():
 
 @app.route('/scan/<token>/<data>')
 def scan(token='', data=''):
-    if verify_input(token):
-        return 'success'
+    if verify_scantoken("scan_token", token):
+        return f'success {data}'
     else:
-        return 'What the fuck!'
+        return 'failure'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
