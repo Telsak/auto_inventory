c5d7c60682b851a16354bb4c8c9d684c4ab26bdc
Simon Olofsson <telsak@hotmail.se>
2022-11-17 09:00:07 +0100

    cnap to itnr lookup

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from openpyxl import load_workbook
 import os, json, requests
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -24,6 +25,31 @@ def ifttt_message(payload, key):
     msg_data['value1'] = payload
     requests.post(f'https://maker.ifttt.com/trigger/notify/with/key/{key}', data=msg_data)
 
+def verify_barcode(payload):
+    if len(payload) == 9:
+        # check if its a CNAP qr code
+        if payload[0:4] == 'CNAP' and payload[4:].isnumeric():
+            return True, 'CNAP'
+        elif payload[0:2] == 'IT' and payload[2:].isnumeric():
+            return True, 'IT'
+        else:
+            return False, ''
+    else:
+        # neither of these, or corrupt data
+        return False, ''
+
+def cnap_to_itnumber(cnapdata):
+    wb = load_workbook(os.path.join(basedir, 'CNAP_utrustning_inventarie.xlsx'))
+    ws = wb['Inventarie']
+    for row in ws['D']:
+        if cnapdata == row.value:
+            it_cell = ws[f'E{row.row}'].value
+            if len(it_cell) > 0:
+                if 'IT' in it_cell:
+                    ifttt_message(it_cell, verify_scantoken('IFTTT_key', 'NULL'))
+    wb.close()
+    return
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -36,13 +62,20 @@ def scan(token='', data=''):
             from flask import current_app
             room = current_app.config['CURRENTROOM'] = data.split(':')[1]
             ifttt_message(f'room changed to {room}', verify_scantoken('IFTTT_key', 'NULL'))
-        elif 'IT' in data:
+        elif 'IT' in data or 'CNAP' in data:
             # ITnr, check it and process if valid
-            return('IT number')
-        elif 'CNAP' in data:
-            # CNAP QR code, check it and process if valid
-            return('CNAP qr code')
-        return f'success {data}'
+            result, verify = verify_barcode(data)
+            if result:
+                ifttt_message(data, verify_scantoken('IFTTT_key', 'NULL'))
+                if verify == 'CNAP':
+                    cnap_to_itnumber(data)
+                    return(f'{verify.upper()} code', data)
+                else:
+                    return(f'{verify.upper()} code', data)
+            else:
+                return(f'token ok: {data} invalid')
+        else:
+            return f'token ok: {data} invalid'
     else:
         return 'failure'
 
