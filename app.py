619af7bb25a6606efe8212ed63f9e0e600c41619
Simon <telsak@gmail.com>
2022-11-17 06:01:19 +0100

    starting

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+def verify_input(data):
+    if len(data) > 0:
+        with open(os.path.join(basedir, 'tk.crd')) as file:
+            if data == file.readline():
+                return True
+    return False
 
 @app.route('/')
 def index():
@@ -8,7 +18,10 @@ def index():
 
 @app.route('/scan/<token>/<data>')
 def scan(token='', data=''):
-    return 'What the fuck!'
+    if verify_input(token):
+        return 'success'
+    else:
+        return 'What the fuck!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
